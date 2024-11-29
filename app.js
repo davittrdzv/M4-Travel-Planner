e142499b71b05c6d8082b563b772fa36bdbfaf97
davittrdzv <davittrdzv@gmail.com>
2024-11-29 15:28:43 -0600

    Commit 7: Ajustes en JS para recibir input del usuario respecto de los viajes, y mostrarlo en el DOM. Se agregan alertas, se agregan propiedades a nuevo viaje.

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 // app.js
 
-// Iniciar la aplicación
-
 import { registrarDestino } from './viajes.js';
-import { mostrarItinerario } from './viajes.js';
+import { form } from './viajes.js';
 
+// Iniciar la aplicación
 const iniciarApp = () => {
-    // Ejemplo de cómo registrar destinos
-    registrarDestino("París", "2024-06-15", "Avión", 1);
-    registrarDestino("Londres", "2024-07-01", "Tren", 2);
-    registrarDestino("Viena", "2024-08-05", "Autobús", 3);
-    registrarDestino("Madrid", "2024-09-10", "Avión", 4);
-    registrarDestino("Barcelona", "2024-10-12", "Tren", 5);
-    registrarDestino("Roma", "2024-11-01", "Avión", 6);
-    registrarDestino("Ámsterdam", "2024-12-02", "Autobús", 7);
-    registrarDestino("Lisboa", "2025-01-15", "Tren", 8);
-    registrarDestino("Bratislava", "2025-02-25", "Avión", 9);
-    registrarDestino("Praga", "2025-03-10", "Autobús", 10);
-
-    // Mostrar el itinerario de los viajes
-    mostrarItinerario();
+    form.addEventListener('submit', registrarDestino);
 }
 
 // Ejecutar la aplicación
 iniciarApp();
+
--- viajes.js
+++ viajes.js
@@ -1,19 +1,65 @@
 // viajes.js
-
-// Array para guardar los destinos
-const destinos = [];
+export const form = document.querySelector('#create-to-do-form');
+const selecDestinos = document.querySelector('#destinos');
+const selecFechaInicio = document.querySelector('#fecha-inicio');
+const selecFechaFin = document.querySelector('#fecha-fin');
+const selecTransporte = document.querySelector('#transporte');
+const selecViajeros = document.querySelector('#viajeros');
+const contenedorViajes = document.querySelector('#contenedor-viajes');
+export const destinos = []; // Array para guardar los destinos
 
 // Función para registrar un destino de viaje
-export const registrarDestino = (destino, fecha, transporte, viajeros) => {
-    // TODO: Crear un objeto con los datos del destino
+export const registrarDestino = (event) => {
+    // Prevenir refresh de la página
+    event.preventDefault();
+
+    // Alertas en caso de no incluir la información
+    if (selecDestinos.value === "") {
+        alert('Por favor incluye el destino');
+        return;
+    }
+    if (selecFechaInicio.value === "") {
+        alert('Por favor incluye la fecha de inicio');
+        return;
+    }
+    if (selecFechaFin.value === "") {
+        alert('Por favor incluye la fecha de fin');
+        return;
+    }
+    if (new Date(selecFechaInicio.value) > new Date(selecFechaFin.value)) {
+        alert('La fecha de inicio no puede ser posterior a la fecha de fin');
+        return;
+    }
+    if (selecTransporte.value === "") {
+        alert('Por favor incluye el medio de transporte');
+        return;
+    }
+    if (selecViajeros.value === "") {
+        alert('Por favor incluye el número de viajeros');
+        return;
+    }
+
+    // variable de nuevo viaje
     const nuevoViaje = {
-        destino: destino,
-        fecha: fecha,
-        transporte: transporte,
-        viajeros: parseInt(viajeros),
-        costo: calcularCosto(destino, transporte, viajeros)
+        destino: selecDestinos.value,
+        fecha_de_inicio: selecFechaInicio.value,
+        fecha_de_fin: selecFechaFin.value,
+        dias_totales: calcularDias(selecFechaInicio.value, selecFechaFin.value),
+        transporte: selecTransporte.value,
+        viajeros: parseInt(selecViajeros.value),
+        costo: calcularCosto(selecDestinos.value, selecTransporte.value, parseInt(selecViajeros.value))
     };
+
+    // Agregar nuevo viaje a viajes totales
     destinos.push(nuevoViaje);
+    
+    // Resetear información de los campos
+    selecDestinos.value = "";
+    selecFechaInicio.value = "";
+    selecFechaFin.value = "";
+    selecTransporte.value = "";
+    selecViajeros.value = "";
+    mostrarItinerario();
 };
 
 // Función para calcular el costo del viaje
@@ -52,31 +98,53 @@ const calcularCosto = (destino, transporte, viajeros, descuento) => {
         costoBase += 60;
     }
 
+    // Descuento con base en número de personas (limitado a 25%)
     descuento = 0;
     if (viajeros > 1) {
         descuento = viajeros * .025;
     }
 
+    // Limitación de descuento 25 %)
     if (descuento > 0.25) {
         descuento = 0.25;
     }
 
-    costoBase = costoBase * viajeros * (1 - descuento)
-
+    costoBase = costoBase * viajeros * (1 - descuento);
     return costoBase;
 }
 
+const calcularDias = (fechaInicio, fechaFin) => {
+    let fecha1 = new Date(fechaInicio);
+    let fecha2 = new Date(fechaFin);
+    
+    fecha1.setHours(0, 0, 0, 0);
+    fecha2.setHours(0, 0, 0, 0);
 
-// Función para mostrar el itinerario de los viajes registrados
+    // Calculating the time difference
+    // of two dates
+    let diferenciaTiempo = fecha2 - fecha1;
+
+    // Calculating the no. of days between
+    // two dates
+    let diferenciaDias = Math.round(diferenciaTiempo / (1000 * 3600 * 24));
+    return diferenciaDias
+}
+
 export const mostrarItinerario = () => {
-    // TODO: Recorrer el arreglo de destinos y mostrar la información de cada uno
+    contenedorViajes.innerHTML = "";
+    
+    // Recorrer el arreglo de destinos y mostrar la información de cada uno
     for (let i = 0; i < destinos.length; i++) {
-        const viaje = destinos[i];
-        console.log(`Destino: ${viaje.destino}
-Fecha: ${viaje.fecha}
-Transporte: ${viaje.transporte}
-Viajeros: ${viaje.viajeros}
-Costo: $${viaje.costo}`
-        )
+        const itemViaje = document.createElement('div');
+        itemViaje.setAttribute('class', 'contenedor');
+        itemViaje.innerHTML =
+        `<span>${destinos[i].destino}</span>
+        <span>${destinos[i].fecha_de_inicio}</span>
+        <span>${destinos[i].fecha_de_fin}</span>
+        <span>${destinos[i].dias_totales}</span>
+        <span>${destinos[i].transporte}</span>
+        <span>${destinos[i].viajeros}</span>
+        <span>$${destinos[i].costo.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>`;
+        contenedorViajes.appendChild(itemViaje);
     }
 }
